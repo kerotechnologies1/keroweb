f13f7da666c7da8e0bcecccdc28f1ab8bbca862f
kerotechnologies1 <kerotechnologies1@gmail.com>
2025-04-11 15:53:51 +0100

    Update rides.jsx

--- src/routes/rides.jsx
+++ src/routes/rides.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useMemo } from "react";
 import { toast } from "react-toastify";
 import { flexRender, getCoreRowModel, getPaginationRowModel, getFilteredRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table";
 import { CheckCircle, Clock, XCircle, Search, MapPin, ChevronUp, ChevronDown, DollarSign, Navigation } from "lucide-react";
-import Modal from "@/components/Modal";
+import Modal from "@/components/modal";
 import RideStatCard from "@/components/RideStatCard";
 import api from "@/utils/api";
 
